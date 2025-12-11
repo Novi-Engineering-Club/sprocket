388f223fc73cac319a02eb85078aa677aa8cade9
smit4k <smit@smit.codes>
2025-12-10 19:05:44 -0500

    Add roll command to roll a die

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,6 +11,19 @@ class Fun(commands.Cog):
         result = ["Heads", "Tails"]
         return random.choice(result)
 
+    async def roll_logic(self, max_value: int = 6) -> int:
+        return random.randint(1, max_value)
+
+    @commands.command(name="roll")
+    async def roll_prefix(self, ctx, max_value: int = 6):
+        result = await self.roll_logic(max_value)
+        await ctx.send(f"You rolled a **{result}**")
+
+    @discord.slash_command(name="roll")
+    async def roll_slash(self, ctx: discord.ApplicationContext, max_value: int = 6):
+        result = await self.roll_logic(max_value)
+        await ctx.respond(f"You rolled a **{result}**")
+
     @commands.command(name="flip")
     async def flip_prefix(self, ctx):
         result = await self.flip_logic()
