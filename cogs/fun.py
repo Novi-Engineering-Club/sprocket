377beb3fa82cfe6a2fdca6873e2935bcb212996d
mastervivalt-droid <mastervivalt@gmail.com>
2025-12-12 20:43:29 +0000

    Add random quote command

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,7 +1,7 @@
 import discord
 import random
 from discord.ext import commands
-
+import requests
 
 class Fun(commands.Cog):
     def __init__(self, bot):
@@ -13,6 +13,12 @@ class Fun(commands.Cog):
 
     async def roll_logic(self, max_value: int = 6) -> int:
         return random.randint(1, max_value)
+    
+
+    async def get_quote(self):
+        response = requests.get('https://zenquotes.io/api/quotes/')
+        data = response.json()
+        return data[0]["q"]
 
     @commands.command(name="roll")
     async def roll_prefix(self, ctx, max_value: int = 6):
@@ -33,6 +39,11 @@ class Fun(commands.Cog):
     async def flip_slash(self, ctx):
         result = await self.flip_logic()
         await ctx.respond(result)
+    
+    @commands.command(name="quote")
+    async def get_quote_prefix(self, ctx):
+        quote = await self.get_quote()
+        await ctx.send(quote)
 
 
 def setup(bot):
