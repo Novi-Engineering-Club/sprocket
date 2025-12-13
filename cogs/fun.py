9df836b04ae400a08ce547d2be4128ae27909394
smit4k <smit@smit.codes>
2025-12-13 09:03:00 -0500

    Replace requests with aiohttp for async HTTP calls

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,24 +1,25 @@
 import discord
 import random
 from discord.ext import commands
-import requests
+import aiohttp
+
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     async def flip_logic(self):
-        result = ["Heads", "Tails"]
-        return random.choice(result)
+        return random.choice(["Heads", "Tails"])
 
     async def roll_logic(self, max_value: int = 6) -> int:
         return random.randint(1, max_value)
-    
 
     async def get_quote(self):
-        response = requests.get('https://zenquotes.io/api/quotes/')
-        data = response.json()
-        return data[0]["q"]
+        url = "https://zenquotes.io/api/quotes/"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                data = await response.json()
+                return data[0]["q"]
 
     @commands.command(name="roll")
     async def roll_prefix(self, ctx, max_value: int = 6):
@@ -39,7 +40,7 @@ class Fun(commands.Cog):
     async def flip_slash(self, ctx):
         result = await self.flip_logic()
         await ctx.respond(result)
-    
+
     @commands.command(name="quote")
     async def get_quote_prefix(self, ctx):
         quote = await self.get_quote()
@@ -49,7 +50,6 @@ class Fun(commands.Cog):
     async def get_quote_slash(self, ctx):
         quote = await self.get_quote()
         await ctx.respond(quote)
-    
 
 
 def setup(bot):
